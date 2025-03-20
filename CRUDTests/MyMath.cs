082673bac7449e771d3316cc8908f139dd503267
Scott <scott.miner.data.scientist@gmail.com>
2025-03-20 11:22:47 -0600

    Implement AddCountry method

---
+++ CRUDTests/MyMath.cs
@@ -0,0 +1,11 @@
+﻿namespace CRUDTests
+{
+    internal class MyMath
+    {
+        public int Add(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+    }
+}
---
+++ Services/CountriesService.cs
@@ -0,0 +1,31 @@
+﻿using Entities;
+
+using ServiceContracts;
+using ServiceContracts.DTO;
+
+namespace Services
+{
+    public class CountriesService : ICountriesService
+    {
+        private readonly List<Country> _countries;
+
+        public CountriesService()
+        {
+            _countries = new List<Country>();
+        }
+
+        public CountryResponse AddCountry(CountryAddRequest? countryAddRequest)
+        {
+            // Convert object from CountryAddRequest to Country type
+            Country country = countryAddRequest.ToCountry();
+
+            // Generate CountryID
+            country.CountryID = Guid.NewGuid();
+
+            // Add country object into _countries
+            _countries.Add(country);
+
+            return country.ToCountryResponse();
+        }
+    }
+}
